316cae88854127f91fbb5358627bcb1a32dd674f
Axe <seamac@gmail.com>
2024-08-30 13:00:15 +0100

    add optional env file args

--- grt.ts
+++ grt.ts
@@ -9,66 +9,60 @@ import Graphinator from './src/graphinator';
 
 const log = (msg: string, lineDecorator="") => console.log(`${new Date().toISOString()} - ${lineDecorator} (Graphinator) ${msg}`);
 
+dotenv.config();
+
 const argv = await yargs(hideBin(process.argv))
     .option('network', {
         alias: 'n',
         type: 'string',
         description: 'Set the network',
-        default: 'base-mainnet'
+        default: process.env.NETWORK || 'base-mainnet'
     })
     .option('batchSize', {
         alias: 'b',
         type: 'number',
         description: 'Set the batch size',
-        default: 15
+        default: process.env.BATCH_SIZE ? parseInt(process.env.BATCH_SIZE) : 15
     })
     .option('gasMultiplier', {
         alias: 'g',
         type: 'number',
         description: 'Set the gas multiplier',
-        default: 1.2
+        default: process.env.GAS_MULTIPLIER ? parseFloat(process.env.GAS_MULTIPLIER) : 1.2
     })
     .option('token', {
         alias: 't',
         type: 'string',
         description: 'Set the token to liquidate',
-        default: undefined
+        default: process.env.TOKEN
     })
     .option('loop', {
         alias: 'l',
         type: 'boolean',
         description: 'Set to true to loop forever, false to run once',
-        default: false
+        default: process.env.LOOP === 'true'
     })
     .option('maxGasPrice', {
         alias: 'm',
         type: 'number',
         description: 'Set the max gas price',
-        default: 500000000
+        default: process.env.MAX_GAS_PRICE ? parseInt(process.env.MAX_GAS_PRICE) : 500000000
     })
     .parse();
 
-const runAgainIn = 30000//15 * 60 * 1000;
+const runAgainIn = 30000 //15 * 60 * 1000;
 const network = argv.network;
 const batchSize = argv.batchSize;
 const gasMultiplier = argv.gasMultiplier;
 const token = argv.token;
 const loop = argv.loop;
-const maxGasPrice = BigInt(argv.maxGasPrice);
-
-
-if (network === undefined) {
-    // TODO: probably not a valid code path
-    dotenv.config();
-} else {
-    dotenv.config({path: path.resolve(__dirname, `.env_${network}`)});
-}
+const maxGasPrice = argv.maxGasPrice;
 
 const ghr = new Graphinator(network);
 if(loop) {
     const executeLiquidations = async () => {
         try {
-            await ghr.executeLiquidations(batchSize, gasMultiplier, token);
+            await ghr.executeLiquidations(batchSize, gasMultiplier, maxGasPrice, token);
         } catch (error) {
             console.error(error);
         } finally {
