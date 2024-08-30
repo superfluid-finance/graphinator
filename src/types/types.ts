3a432ca0e4ea4b1a147d218621f8a9734db8eb63
Axe <seamac@gmail.com>
2024-08-30 13:00:35 +0100

    clean up types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,27 +1,15 @@
 import type {AddressLike} from "ethers";
 
-type LiquidationParams = {
-    source: AddressLike,
+export type LiquidationParams = {
+    source: string,
     sender: AddressLike,
     receiver: AddressLike
 }
 
 export type CriticalAccount = {
     id: AddressLike,
-    balanceUntilUpdatedAt: number,
-    maybeCriticalAtTimestamp: number,
-    isLiquidationEstimateOptimistic: boolean,
-    activeIncomingStreamCount: number,
-    activeOutgoingStreamCount: number,
-    activeGDAOutgoingStreamCount: number,
-    activeCFAOutgoingStreamCount: number,
-    totalInflowRate: number,
-    totalOutflowRate: number,
     totalNetFlowRate: number,
-    totalCFAOutflowRate: number,
     totalCFANetFlowRate: number,
-    totalGDAOutflowRate: number,
-    totalDeposit: number,
     token: Token,
     account: {
         id: AddressLike
